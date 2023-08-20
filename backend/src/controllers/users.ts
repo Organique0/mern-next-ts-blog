cc87ec2eaf294a2a0ffc0737eeee99e2b9ee9f1c
Organique0 <grabnar.luka@gmail.com>
2023-08-21 00:53:37 +0200

    email verification done

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -3,13 +3,17 @@ import UserModel from "../models/user";
 import createHttpError from "http-errors";
 import bcrypt from "bcrypt";
 import assertIsDefined from "../utils/assertIsDefined";
-import { UpdateUserBody } from "../validation/users";
+import { RequestVerificationCodeBody, UpdateUserBody } from "../validation/users";
 import sharp from "sharp";
 import env from "../env";
+import crypto from "crypto";
+import EmailVerToken from "../models/email-ver-token";
+import * as Email from "../utils/email";
 interface signUpBody {
     username: string,
     email:string,
     password:string,
+    verificationCode:string,
 }
 
 export const getUserByUsername:RequestHandler = async (req,res,next) => {
@@ -24,13 +28,43 @@ export const getUserByUsername:RequestHandler = async (req,res,next) => {
     }
 }
 
+export const requestVerificationCode:RequestHandler<unknown,unknown,RequestVerificationCodeBody,unknown> = async (req,res,next) => {
+    const {email} = req.body;
+
+    try {
+        const existingEmail = await UserModel.findOne({email}).collation({locale:"en", strength:2}).exec();
+
+        if(existingEmail) {
+            throw createHttpError(409, "A user with this email address already exists. Please log in instead.")
+        }
+
+        const verificationCode = crypto.randomInt(100000, 999999).toString();
+
+        await EmailVerToken.create({email, verificationCode});
+
+        await Email.sendVerificationCode(email, verificationCode)
+
+        res.sendStatus(200);
+    } catch (error) {
+        next(error);
+    }
+}
+
 export const signUp: RequestHandler<unknown, unknown, signUpBody, unknown> = async (req,res,next) => {
-    const {username, email, password} = req.body;
+    const {username, email, password, verificationCode} = req.body;
     try {
         const existingUsername = await UserModel.findOne({username}).collation({ locale: "en", strength:2 }).exec();
 
         if(existingUsername) {
             throw createHttpError(409, "username taken");
+        }
+
+        const emailVerToken = await EmailVerToken.findOne({email, verificationCode}).exec();
+
+        if(!emailVerToken) {
+            throw createHttpError(400, "verification code incorrect or expired");
+        } else {
+            await emailVerToken.deleteOne();
         }
 
         const passwordHashed = await bcrypt.hash(password,10);
