16ee252a50ee8d03e5941de5b8630a304f5a3488
Organique0 <grabnar.luka@gmail.com>
2023-08-24 04:51:33 +0200

    e

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,5 +1,5 @@
 import passport from "passport";
-import {Strategy as LocalStrategy} from "passport-local";
+import { Strategy as LocalStrategy } from "passport-local";
 import UserModel from "../models/user";
 import bcrypt from "bcrypt";
 import mongoose from "mongoose";
@@ -12,21 +12,21 @@ passport.serializeUser((user, cb) => {
     cb(null, user._id)
 });
 
-passport.deserializeUser((userId:string, cb) => {
+passport.deserializeUser((userId: string, cb) => {
     cb(null, { _id: new mongoose.Types.ObjectId(userId) });
 })
 
 passport.use(new LocalStrategy(async (username, password, cb) => {
     try {
-        const existingUser = await UserModel.findOne({username}).select("+email +password").exec();
+        const existingUser = await UserModel.findOne({ username }).select("+email +password").exec();
 
-        if(!existingUser || !existingUser.password) {
+        if (!existingUser || !existingUser.password) {
             return cb(null, false);
         }
 
         const passwordMatch = await bcrypt.compare(password, existingUser.password);
 
-        if(!passwordMatch) {
+        if (!passwordMatch) {
             return cb(null, false);
         }
 
@@ -42,15 +42,15 @@ passport.use(new LocalStrategy(async (username, password, cb) => {
 
 
 passport.use(new GoogleStrategy({
-    clientID:env.GOOGLE_CLIENT_ID,
-    clientSecret:env.GOOGLE_CLIENT_SECRET,
+    clientID: env.GOOGLE_CLIENT_ID,
+    clientSecret: env.GOOGLE_CLIENT_SECRET,
     callbackURL: env.SERVER_URL + "/users/oauth2/redirect/google",
     scope: ["profile"],
-}, async(accessToken, refreshToken, profile, cb) => {
+}, async (accessToken, refreshToken, profile, cb) => {
     try {
-        let user = await UserModel.findOne({googleId: profile.id}).exec();
+        let user = await UserModel.findOne({ googleId: profile.id }).exec();
 
-        if(!user) {
+        if (!user) {
             user = await UserModel.create({
                 googleId: profile.id,
             });
@@ -58,7 +58,7 @@ passport.use(new GoogleStrategy({
 
         cb(null, user);
     } catch (error) {
-        if(error instanceof Error) {
+        if (error instanceof Error) {
             cb(error);
         } else {
             throw error;
@@ -69,13 +69,13 @@ passport.use(new GoogleStrategy({
 
 passport.use(new GithubStrategy({
     clientID: env.GITHUB_CLIENT_ID,
-    clientSecret:env.GITHUB_CLIENT_SECRET,
-    callbackURL:"/users/oauth2/redirect/github",
-},async (accessToken:string, refreshToken:string, profile:Profile, cb:VerifyCallback) => {
+    clientSecret: env.GITHUB_CLIENT_SECRET,
+    callbackURL: env.SERVER_URL + "/users/oauth2/redirect/github",
+}, async (accessToken: string, refreshToken: string, profile: Profile, cb: VerifyCallback) => {
     try {
-        let user = await UserModel.findOne({githubId: profile.id}).exec();
+        let user = await UserModel.findOne({ githubId: profile.id }).exec();
 
-        if(!user) {
+        if (!user) {
             user = await UserModel.create({
                 githubId: profile.id,
             });
@@ -83,7 +83,7 @@ passport.use(new GithubStrategy({
 
         cb(null, user);
     } catch (error) {
-        if(error instanceof Error) {
+        if (error instanceof Error) {
             cb(error);
         } else {
             throw error;
