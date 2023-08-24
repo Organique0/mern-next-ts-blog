03cd5622c68262c19507fbb74061d8af482ac5bd
Organique0 <grabnar.luka@gmail.com>
2023-08-24 23:06:18 +0200

    added s3 domains

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,8 +4,10 @@ const nextConfig = {
   images: {
     domains: [
       "localhost",
-      "https://next-mern-ts-blog-deca06f16114.herokuapp.com/"
-    ], //allow image loading from localhost
+      "https://next-mern-ts-blog-deca06f16114.herokuapp.com/",
+      "mern-next-ts-blog.s3.amazonaws.com",
+      "mern-next-ts-blog.s3.eu-north-1.amazonaws.com"
+    ],
     deviceSizes: [576, 768, 992, 1200, 1400] //bootstrap sizes
   }
 };
