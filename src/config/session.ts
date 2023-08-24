1a1110d8c6a6527769bf7b1c64c898763e36f581
Organique0 <grabnar.luka@gmail.com>
2023-08-24 19:50:06 +0200

    i does not work

--- src/config/session.ts
+++ src/config/session.ts
@@ -6,6 +6,8 @@ import { CookieOptions } from "express";
 
 const cookieConfig: CookieOptions = {
     maxAge: 7 * 24 * 60 * 60 * 1000,
+    secure: true,
+    sameSite: "none",
 }
 
 if (env.NODE_ENV === "production") {
