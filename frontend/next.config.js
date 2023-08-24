fa88096ff359701d2c0d5de389d210ec8412c5ab
Organique0 <grabnar.luka@gmail.com>
2023-08-24 04:57:08 +0200

    sa

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost"], //allow image loading from localhost
+    domains: [
+      "localhost",
+      "https://next-mern-ts-blog-deca06f16114.herokuapp.com"
+    ], //allow image loading from localhost
     deviceSizes: [576, 768, 992, 1200, 1400] //bootstrap sizes
   }
 };
